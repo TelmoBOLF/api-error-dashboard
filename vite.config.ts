4f15779c3a4dce2a8497ba905b9b001f6a190313
Telmo Barros <tb@onlinefuels.de>
2025-05-07 18:54:16 +0200

    fix: tailwind styles not working

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(), 
-    tailwindcss()
   ],
+  css: {
+    postcss: {
+      plugins: [
+        tailwindcss,
+        autoprefixer,
+      ],
+    },
+  },
 });
